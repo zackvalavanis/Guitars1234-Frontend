8ac335c73130b0f48fbfe2005f63bb20ff40002e
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-28 12:59:09 -0500

    added logout functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import { Footer } from './Footer';
 import { GuitarsPage } from './GuitarsPage';
 import { SignupPage } from './SignupPage';
 import { LoginPage } from './LoginPage';
-
+import { LogoutLink } from './LogoutLink'
 function App () { 
   return (
     <main>
+      <LogoutLink />
       <Header />
       <SignupPage />
       <LoginPage />
---
+++ src/LogoutLink.jsx
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+export function LogoutLink () { 
+  const handleClick = (event) => { 
+    event.preventDefault();
+    delete axios.defaults.headers.common["Authorization"];
+    localStorage.removeItem("jwt");
+    window.location.href = "/";
+  }
+  return (
+    <a href='#' onClick={handleClick}>
+      Logout
+    </a>
+  )
+}
