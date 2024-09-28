7b0924dc58e828877e45f514e6011252a0913078
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-28 13:18:25 -0500

    added functionality for multiple images with default

--- src/GuitarsIndex.jsx
+++ src/GuitarsIndex.jsx
@@ -6,6 +6,9 @@ export function GuitarsIndex ({guitars}) {
         <div key={guitar.id}>
           <h2>{guitar.name}</h2>
           <h2>{guitar.price}</h2>
+          {guitar.images_with_default.map(image => (
+            <img key={image.id} src={image.url} />
+          ))}
         </div>
       ))}
     </div>
