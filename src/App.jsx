944eccc45fc3dec806b67e8b19ecea28ab8acd87
Zack <168221270+Zval321@users.noreply.github.com>
2024-09-28 12:39:08 -0500

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { Header } from './Header'
-import { Footer } from './Footer'
-import { GuitarsPage } from './GuitarsPage'
-import { SignupPage } from './SignupPage'
+import { Header } from './Header';
+import { Footer } from './Footer';
+import { GuitarsPage } from './GuitarsPage';
+import { SignupPage } from './SignupPage';
+import { LoginPage } from './LoginPage';
 
 function App () { 
   return (
     <main>
       <Header />
       <SignupPage />
+      <LoginPage />
       <GuitarsPage />
       <Footer />
     </main>
